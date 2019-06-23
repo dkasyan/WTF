67654672e3bc4e484dd9c6342ac841d4bbf0e924
dkasyan <damian.kasyan@gmail.com>
2019-06-23 17:33:54 +0200

    js file and queryselector

---
+++ homepage/main.js
@@ -0,0 +1,9 @@
+const name = "Damian"
+const age = 33;
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+const about = document.querySelector('.paragraph__Main--js');
+about.innerHTML = `Nazywam się ${name} i mam ${age} lat`;
+
+
+
