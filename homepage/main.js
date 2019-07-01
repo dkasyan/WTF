183e095c0e70420ac8b3f1f9d465dc1d27cd76ca
dkasyan <damian.kasyan@hotmail.com>
2019-07-01 20:35:48 +0200

    Add imie js function

--- homepage/main.js
+++ homepage/main.js
@@ -5,5 +5,8 @@ console.log(`Nazywam się ${name} i mam ${age} lat`);
 const about = document.querySelector('.paragraph__Main--js');
 about.innerHTML = `Nazywam się ${name} i mam ${age} lat`;
 
-
+function imie(name, age){
+    alert(`Czesc ${name} masz ${age} lat!!!`);
+}
+const przywitanie = imie(name, age);
 
